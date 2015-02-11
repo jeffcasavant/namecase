b3b74b1c5f302b199837c26575323b23a7830a34
Jeff Casavant <jeff.casavant@gmail.com>
2015-02-11 15:38:58 -0500

    Initial iteration of v2

--- approaches/v2-titlecase-exceptions.py
+++ approaches/v2-titlecase-exceptions.py
@@ -5,26 +5,112 @@ import argparse
 
 from titlecase import titlecase
 
-#### Handle different methods of input
+def standardizeName(name):
+    name = titlecase(name)
 
-parser = argparse.ArgumentParser()
+    ## Generic rules applied first so they can be overridden
+    # Arbitrary dashes, e.g. Smythington-Smythe-Smith
+    newName = []
+    for namePart in name.split():
+        if '-' in namePart:
+            namePart = titlecase(namePart.replace('-', ' ')).replace(' ', '-')
+        newName.append(namePart)
 
-# This allows us to take input from a file or from stdin (defaults to stdin)
-parser.add_argument('--input',
-                    type=argparse.FileType('r'),
-                    default='-',
-                    help='Input stream/file')
+    name = ' '.join(newName)
 
-parser.add_argument('--debug',
-                    action='store_true',
-                    default=False)
+    # All of these particles & prefixes should be written here with proper capitalization
+    # http://en.wikipedia.org/wiki/Nobiliary_particle
+    # http://stylemanual.ngs.org/home/D/de-d-etc
+    # Assumptions:
+    #  - If a particle can be written both ways, I have chosen to use the lowercase (which shows that we know it's not a middle name)
 
-args = parser.parse_args()
+    # Since prefixes will also match particles, apply them next so they can be overridden
 
-####
+    prefixes = ["Mac",      # Amy MacDonald                 -- note we will get this one wrong; she uses Amy Macdonald
+                "Mc",       # Ronald McDonald
+                "d'",       # Ferdinand d'Orleans
+                "l'",       # Pierre l'Enfant
+                "O'",       # Peter O'Toole
+                "al-"]      # Haroun al-Rasheed
 
-for line in args.input.readlines():
-    line = titlecase(line)
-    
-    upper_exceptions = ['mac']
-    lower_exceptions = ['von']
+
+    newName = []
+    for namePart in name.split():
+        for prefix in prefixes:
+            # If the prefix is the first thing in the namePart
+            if prefix.lower() == namePart.lower()[:len(prefix)]:
+                # Remove the prefix
+                namePart = namePart.lower()[len(prefix):]
+                # Capitalize the next letter
+                namePart = namePart.title()
+                # Add the prefix back again
+                namePart = prefix + namePart
+        newName.append(namePart)
+
+    name = ' '.join(newName)
+
+    # Particles last since they are the most specific
+
+    particles = ["van",     # Netherlands
+                 "tot",     #
+                 "thoe",    #
+                 "aw",      # Somalia
+                 "der",     # James van der Beek
+                 "von",     # Ludwig von Beethoven
+                 "zu",      # Gottfried Heinrich Graf zu Pappenheim
+                 "di",      # Emilia di Martino
+                 "da",      # Leonardo da Vinci
+                 "de",      # Guy de Maupassant
+                 "do",      # Portuguese alternate forms of 'de'
+                 "dos",     #
+                 "da",      #
+                 "das",     #
+                 "bin",     # Osama bin Laden
+                 "ben",     # Ahmed ben Bella
+                 "bint",    # 
+                 "binte",   #
+                 "ibn",     # Hind ibn Sheik                -- this should technically not show up as part of a full name; but if it does we keep it
+                 "abu",     #                               -- also unlikely 
+                 "af",      # Bredo von Munthe af Morgenstierne
+                 "av",      # Modern Swedish af
+                 "du",      # Bertrand du Guesclin
+                 "na",      # Thailand
+                 "la",      #
+                 "Le",      # French alternate forms of 'la'
+                 "Les"]     #
+
+    newName = []
+    name = name.split()
+    for namePart in name:
+        for particle in particles:
+            # This will only handle already separated particles; Luther Vandross is not Luther van Dross
+            # Also, do not consider it a particle if it is the first or last word (Van Morrison)
+            if particle.lower() == namePart.lower() and namePart is not name[0] and namePart is not name[-1]:
+                namePart = particle
+        newName.append(namePart)
+
+    name = ' '.join(newName)
+
+    return name
+
+if __name__ == '__main__':
+
+    #### Handle different methods of input
+
+    parser = argparse.ArgumentParser()
+
+    # This allows us to take input from a file or from stdin (defaults to stdin)
+    parser.add_argument("--input",
+                        type=argparse.FileType("r"),
+                        default="-",
+                        help="Input stream/file")
+
+    parser.add_argument("--debug",
+                        action="store_true",
+                        default=False)
+
+    args = parser.parse_args()
+
+    #### Parse input
+    for line in args.input.readlines():
+        print standardizeName(line)
